1acaf8be9fb2f99297b871119e96bf55df1b80d7
duongbaduy <riox.doubled@gmail.com>
2024-10-09 14:24:33 +0700

    update cart UI

--- Website-Yody/resources/views/cart/components/cart-empty.blade.php
+++ Website-Yody/resources/views/cart/components/cart-empty.blade.php
@@ -1,4 +1,4 @@
-<section class="flex text-center">
+<section class=" w-full text-center">
     <div class="grid grid-flow-row">
         <img alt="Ảnh giỏ hàng trống" loading="lazy" width="500" height="500" decoding="async" data-nimg="1" class=" mx-auto object-cover" src="https://m.yodycdn.com/web/prod/_next/static/media/cart-empty.250eba9c.svg" style="color: transparent;">  
         <div class=" font-old-standard">
